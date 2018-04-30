07b3220f73550f1474896ebb05fd49aff48f4d39
thbe <project@bendler-net.de>
2018-04-30 09:55:51 +0200

    Fix Travis CI build

--- spec/classes/bacula_spec.rb
+++ spec/classes/bacula_spec.rb
@@ -6,7 +6,7 @@ describe 'bacula', :type => :class do
       let(:facts) do
         facts.merge( { lsbdistid: 'CentOS', lsbmajdistrelease: '7', root_home: '/root', staging_http_get: 'curl' } )
       end
-      let(:params) { { type_fd: true, type_sd: true, type_dir: true, backup_clients: [ 'client01.example.local', 'client02.example.local' ] } }
+      let(:params) { { type_fd: true, type_sd: true, type_dir: true, backup_clients: ['client01.example.local','client02.example.local'] } }
 
       it { is_expected.to compile.with_all_deps }
 
